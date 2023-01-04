3f399a641e7e8f3a181910bd03f05f54275f0984
shawncocklin <shawncocklin@gmail.com>
2023-01-04 17:30:54 -0500

    create player script

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+  private static T _instance;
+
+  public static T Instance
+  {
+    get
+    {
+      return _instance;
+    }
+  }
+
+  protected virtual void Awake()
+  {
+    if (_instance == null)
+    {
+      _instance = this as T;
+    }
+    else
+    {
+      Destroy(gameObject);
+    }
+  }
+}
